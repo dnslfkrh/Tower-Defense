2375c010b1a487b0b31c42aa382134da51f00938
yonguk <lyw3059@gmail.com>
2024-09-08 01:36:28 +0900

    feat: tile map + enemy spawn

---
+++ tower-defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int wayPointCount;
+    private Transform[] wayPoints;
+    private int currentIndex = 0;
+    private Movement2D movement2D;
+
+    public void Setup(Transform[] wayPoints)
+    {
+        movement2D = GetComponent<Movement2D>();
+
+        wayPointCount = wayPoints.Length;
+        this.wayPoints = new Transform[wayPointCount];
+        this.wayPoints = wayPoints;
+
+        transform.position = wayPoints[currentIndex].position;
+
+        StartCoroutine("OnMove");
+    }
+
+    public IEnumerator OnMove()
+    {
+        NextMoveTo();
+
+        while (true)
+        {
+            if (Vector3.Distance(transform.position, wayPoints[currentIndex].position) < 0.02f * movement2D.MoveSpeed)
+            {
+                NextMoveTo();
+            }
+
+            yield return null;
+        }
+    }
+
+    private void NextMoveTo()
+    {
+        if (currentIndex < wayPointCount - 1)
+        {
+            currentIndex++;
+
+            Vector3 direction = (wayPoints[currentIndex].position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
