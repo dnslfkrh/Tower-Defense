d0593dc676bc9608a9209fbddb8a396f7de0083a
yongwuk <lyw3059@gmail.com>
2024-10-19 15:29:43 +0900

    style: fix class name

--- food-protector/Assets/Scripts/Tower/Towers/Bow.cs
+++ food-protector/Assets/Scripts/Tower/Towers/Bow.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class Archer : MonoBehaviour, ITower
+public class Bow : MonoBehaviour, ITower
 {
     [SerializeField]
     private Transform spawnPoint;
